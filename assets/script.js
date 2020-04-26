36b6c65528e5fe91975765f59bdcb542a885771a
Jakenbake20 <jacob_wheat@yahoo.com>
2020-04-26 04:52:54 -0600

    finished up media querries began on jQuery smooth scroll functions.

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
         $('.top-nav').toggleClass('open');
 
     });
+
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+
+    });
+
+    $('nav a[href*="#"]').on('click', function(){
+        $('html, body').animate( {
+            scrollTop: $($(this).attr('href')).offset().top -100
+        }, 20000);
+
+    });
 });
